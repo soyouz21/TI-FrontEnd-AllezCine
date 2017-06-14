eb9a506307fb76b69ceec8581b71961c94a2df98
Mesmaeker <mike-mesmaeker@hotmail.com>
2017-06-14 16:43:33 +0200

    Revert "rajout du 14 juin"

--- site/javascript/js.js
+++ site/javascript/js.js
@@ -1,12 +1,12 @@
-$(function () {
-    $(".dropdown").hover(
-        function () {
-            $('.dropdown-menu', this).stop(true, true).fadeIn("fast");
+$(function(){
+$(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
             $(this).toggleClass('open');
             $('b', this).toggleClass("caret caret-up");
         },
-        function () {
-            $('.dropdown-menu', this).stop(true, true).fadeOut("fast");
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
             $(this).toggleClass('open');
             $('b', this).toggleClass("caret caret-up");
         });
@@ -15,85 +15,21 @@ $(function () {
 // code du carrousel de la section shop
 
 $(document).ready(function () {
-    $('#myCarousel').carousel({
-        interval: 10000
-    })
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
+    	$('#myCarousel').carousel({
+			interval: 10000
+		})
+		$('.fdi-Carousel .item').each(function () {
+			var next = $(this).next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
 
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        } else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
-});
-
-
-
-
-//BOUTON "Plus de films",fonction pour faire apparaître la 3 row au click du bouton "plus de films"
-
-function buttonDisplay(classe, valeur) {
-    var lesFilms = document.getElementsByClassName(classe);
-    for (var i = 0; i < lesFilms.length; ++i) {
-        lesFilms[i].style.display = valeur;
-    }
-}
-
-document.getElementById('ALL').onclick = function () {
-    buttonDisplay('films', 'block');
-}
-document.getElementById('ACTIONS').onclick = function () {
-    buttonDisplay('films', 'none');
-    buttonDisplay('ACTIONS', 'block');
-}
-document.getElementById('HORREUR').onclick = function () {
-    buttonDisplay('films', 'none');
-    buttonDisplay('HORREUR', 'block');
-}
-
-document.getElementById('KIDS').onclick = function () {
-    buttonDisplay('films', 'none');
-    buttonDisplay('KIDS', 'block');
-}
-document.getElementById('SUSPENSE').onclick = function () {
-    buttonDisplay('films', 'none');
-    buttonDisplay('SUSPENSE', 'block');
-}
-
-
-//BOUTON qui fait apparaître ma 3row au click du bouton plus de films.
-//BOUTON "moins de films" qd on click sur "plus de films" 
-$(".3row").hide()
-$("#moinsdefilms").hide()
-$("#plusdefilms").click(function () {
-
-    $(".3row").show("slow");
-    $("#moinsdefilms").show();
-    $("#plusdefilms").hide();
-
-});
-
-$("#moinsdefilms").click(function(){
-    $(".3row").hide("slow");
-    $("#plusdefilms").show();
-    $("#moinsdefilms").hide();
-    
-})
-
-
-
-
-
-
-
-
-/*
-       
-   
-*/
+			if (next.next().length > 0) {
+				next.next().children(':first-child').clone().appendTo($(this));
+			}
+			else {
+				$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+			}
+		});
+	});
