66a2d849b77372bce925fba55f22d5c8fd5dac06
michael mesmaeker <you@example.com>
2017-06-21 22:13:43 +0200

    modif du 21soir

--- site/javascript/mickey.js
+++ site/javascript/mickey.js
@@ -1,4 +1,1 @@
-$(label).append(){
-    if $(this).css("background color","yellow");
-}
-//ici Mike j'ai essayé de commencer le js pour "ce champ doit renseigné" mais pas terminé
+$(".mickeythank").hide();
